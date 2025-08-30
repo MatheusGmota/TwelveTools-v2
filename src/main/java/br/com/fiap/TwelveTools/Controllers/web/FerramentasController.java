020d42eb21a10e0bc0600d3372eb3d308520ecc0
MatheusGmota <livematgreg@gmail.com>
2025-08-30 16:32:30 -0300

    feat: criado controller p/ web

--- src/main/java/br/com/fiap/TwelveTools/Controllers/web/FerramentasController.java
+++ src/main/java/br/com/fiap/TwelveTools/Controllers/web/FerramentasController.java
@@ -1,9 +1,36 @@
 package br.com.fiap.TwelveTools.Controllers.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.fiap.TwelveTools.Service.FerrarmentasService;
+import br.com.fiap.TwelveTools.dtos.FerramentaDTO;
+import br.com.fiap.TwelveTools.model.Ferramenta;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController("webFerramentasController")
 @RequestMapping("/ferramentas")
 public class FerramentasController {
+
+    @Autowired
+    public FerrarmentasService service;
+
+    @GetMapping
+    public String listarFerramentas(Model model) {
+        model.addAttribute("ferramentas", service.getAll());
+        return "listar";
+    }
+
+    @GetMapping
+    public String novaFerramenta(Model model) {
+        model.addAttribute("ferramentas", new Ferramenta());
+        return "formulario";
+    }
+
+    @PostMapping()
+    public String salvarFerramenta(@Valid @RequestBody FerramentaDTO entidade) {
+        service.post(entidade);
+        return "redirect:/ferramentas";
+    }
 }
